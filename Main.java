84f206ad1ac56547fd5824b9164cbf1fc55b6eb8
LivanMDC <113356765+LivanMiranda@users.noreply.github.com>
2022-11-17 19:49:51 +0000

    Try6

--- Main.java
+++ Main.java
@@ -22,30 +22,30 @@ class Main {
     Scanner scanner = new Scanner(System.in);
     
     do {
-      System.out.println(" Press 1 to add an item.");
-      System.out.println(" Press 2 to delete an item.");
-      System.out.println(" Press 3 to update an item.");
-      System.out.println(" Press 4 to show all the items.");
-      System.out.println(" Press 5 to quit the program.");
+      System.out.println("Press 1 to add an item.");
+      System.out.println("Press 2 to delete an item.");
+      System.out.println("Press 3 to update an item.");
+      System.out.println("Press 4 to show all the items.");
+      System.out.println("Press 5 to quit the program.");
 
       options = scanner.nextInt();
       scanner.nextLine();
       switch(options){
         case 1:
-        System.out.println(" Enter the name:");
+        System.out.println("Enter the name:");
         name = scanner.nextLine();
 
-        System.out.println(" Enter serial number:");
+        System.out.println("Enter serial number:");
         serialN = scanner.nextLine();
 
-        System.out.println(" Enter the value in dollars (whole number):");
+        System.out.println("Enter the value in dollars (whole number):");
         value = Integer.parseInt(scanner.nextLine());
         itemList = new Inventory(name, serialN, value);
         item.add(itemList);
       break;
           
       case 2:
-        System.out.println(" Enter the serial number of the item to delete:");
+        System.out.println("Enter the serial number of the item to delete:");
         serialN = scanner.nextLine();
 
         for (int i = 0; i < item.size(); i++) {
@@ -62,9 +62,9 @@ class Main {
 					scanner.nextLine();
 					for (int i = 0; i < item.size(); i++) {
 						if (item.get(i).serialN.equals(st)) {
-							System.out.println(" Enter the new name:");
+							System.out.println("Enter the new name:");
 							String na = scanner.nextLine();
-							System.out.println(" Enter the new value in dollars (whole number):");
+							System.out.println("Enter the new value in dollars (whole number):");
 							String va = scanner.next();
 							item.get(i).name = na;
 							item.get(i).value = Integer.parseInt(va);
@@ -74,7 +74,7 @@ class Main {
           
           case 4:
            for (int i = 0 ; i < item.size(); i++ ){
-               System.out.print(item.get(i).name + ",");
+               System.out.print(" " +item.get(i).name + ",");
                System.out.print(item.get(i).serialN + ",");
                System.out.println(item.get(i).value);
             

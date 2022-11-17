cbde5c60df95350c0f4be07b64a10429cc964e8a
LivanMDC <113356765+LivanMiranda@users.noreply.github.com>
2022-11-17 08:53:07 +0000

    First try

--- Main.java
+++ Main.java
@@ -2,11 +2,99 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Inventory {
+  String name, serialN;
+  int value;
 
+  public Inventory(String name, String serialN, int value) {
+    this.name = name;
+    this.serialN = serialN;
+    this.value = value;
+
+  }
+
+  public void setname(String name) {
+    this.name = name;
+
+  }
+
+  public void setvalue(int value) {
+    this.value = value;
+
+  }
+
+  public String getname() {
+    return this.name;
+  }
+
+  public String getserialN() {
+    return this.serialN;
+  }
+
+  public int getvalue() {
+    return this.value;
+  }
+  public String tostring(){
+    return name + "," + serialN + ","+ String.valueOf(value);
+  }
 }
 
 class Main {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    int options = 0, value;
+    String name, serialN;
+    ArrayList<Inventory> item = new ArrayList<Inventory>();
+    Inventory itemList;
+    Scanner scanner = new Scanner(System.in);
+    do {
+      System.out.println("Press 1 to add an item");
+      System.out.println("Press 2 to delete an item");
+      System.out.println("Press 3 to update an item");
+      System.out.println("Press 4 to show all the items");
+      System.out.println("Press 5 to quit the program");
 
-	}
+      options = scanner.nextInt();
+      if (options == 1) {
+        System.out.println("Enter the name:");
+        name = scanner.next();
+
+        System.out.println("Enter serial number:");
+        serialN = scanner.next();
+
+        System.out.println("Enter the value in dollars (whole number):");
+        value = scanner.nextInt();
+        itemList = new Inventory(name, serialN, value);
+        item.add(itemList);
+      }
+      if (options == 2) {
+        System.out.println("Enter the serial number of the item to delete:");
+        serialN = scanner.next();
+
+        for (int i = 0; i < item.size(); i++) {
+          if (item.get(i).serialN.equals(serialN)) {
+            item.remove(i);
+          }
+        }
+      }
+      if (options == 3) {
+        System.out.println("Enter the serial number of the item to change:");
+        serialN = scanner.next();
+        for (int i = 0; i < item.size(); i++) {
+          if (item.get(i).getserialN().equals(serialN)) {
+            System.out.println("Enter the new name:");
+            name = scanner.next();
+            item.get(i).setname(name);
+            System.out.println("Enter the new value in dollars (whole numbers):");
+            value = scanner.nextInt();
+            item.get(i).setvalue(value);
+          }
+        }
+      }
+      if(options == 4){
+        for(int i =0; i < item.size(); i++){
+          System.out.println(item.get(i).tostring());
+            
+        }
+      }
+    } while (options != 5);
+  }
 }
